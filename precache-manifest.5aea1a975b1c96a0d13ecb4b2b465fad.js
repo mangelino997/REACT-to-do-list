f554887548e40de0e48131f201ffcff862b8b486
Marcio <marcio@draimo.com>
2020-09-04 21:14:23 -0300

    Updates

---
+++ precache-manifest.5aea1a975b1c96a0d13ecb4b2b465fad.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7ad0fab95ecb1bf0fc23481435cc09",
+    "url": "/REACT-to-do-list/index.html"
+  },
+  {
+    "revision": "a74efd2b99f020b71dcd",
+    "url": "/REACT-to-do-list/static/css/2.5e34d011.chunk.css"
+  },
+  {
+    "revision": "c9d2d4d8a14cf55db383",
+    "url": "/REACT-to-do-list/static/css/main.a045082d.chunk.css"
+  },
+  {
+    "revision": "a74efd2b99f020b71dcd",
+    "url": "/REACT-to-do-list/static/js/2.e247c325.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/REACT-to-do-list/static/js/2.e247c325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9d2d4d8a14cf55db383",
+    "url": "/REACT-to-do-list/static/js/main.e75ecb1f.chunk.js"
+  },
+  {
+    "revision": "86ac5fecdc4ed1122007",
+    "url": "/REACT-to-do-list/static/js/runtime-main.21b42c7a.js"
+  },
+  {
+    "revision": "7bdeba5609e6782ca0e5a297ee7750e4",
+    "url": "/REACT-to-do-list/static/media/bg4.7bdeba56.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/REACT-to-do-list/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/REACT-to-do-list/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/REACT-to-do-list/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/REACT-to-do-list/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/REACT-to-do-list/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
